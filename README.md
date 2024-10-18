eb0cd908566b68b5c9c75ea241104982859f7459
Ryanch <zhgrun.d@gmail.com>
2024-10-18 09:54:38 +0800

    add static build option

--- README.md
+++ README.md
@@ -11,13 +11,18 @@ Please run following command to build
 ```
 ./build.sh
 ```
-For more options for building, please check build script for details.
+By default, it will build with dynamic link. If you want a static build, please run
+```
+./build.sh --static
+
+```
+For more options, please check the build script for details.
 
 ## How to use
 `clangtool` is a standalone command line tool, similar as offical LLVM tool `clang-check`.
 
 `clangtool` can parse and analyze c/c++ source files via clang AST based on LLVM clang Tooling infrastructure.
 
-An example using `clangtool` is integrated in `CMakeLists.txt`, it extracts function declarationsin c/c++ header file, please check for more details.
+An example using `clangtool` is integrated in `CMakeLists.txt`, it extracts function declarations in c/c++ header file, please check for more details.
 
 This is only for demonstration, you can modify or extend as you want.
--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ build_help()
 
         echo -e "options:\n"
         echo -e "  -llvm,  --llvm              Build llvm project only and exit"
+        echo -e "  -static,  --static          Enable static build, disabled by default."
         echo -e "  -cxx,  --cxx                Source language is C++, default C."
         echo -e "  -h,  --help                 Prints this help"
 }
@@ -22,24 +23,39 @@ build_llvm()
     if [ -d "$BUILD_DIR" ]; then
         ninja -C $BUILD_DIR install
     else
-        cmake -G "Ninja" \
-            -S ${LLVM_SOURCE_DIR}/llvm \
-            -B $BUILD_DIR \
-            -DLLVM_ENABLE_PROJECTS="clang" \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DLLVM_TARGETS_TO_BUILD="X86" \
-            -DCMAKE_CXX_COMPILER=clang++ \
-            -DCMAKE_C_COMPILER=clang \
-            -DLLVM_BUILD_LLVM_DYLIB=ON \
-            -DLLVM_LINK_LLVM_DYLIB=ON \
-            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    	    -DCMAKE_INSTALL_PREFIX=${BUILD_DIR}/install
+        if [ "$ENABLE_STATIC" = "yes" ]; then
+            cmake -G "Ninja" \
+                -S ${LLVM_SOURCE_DIR}/llvm \
+                -B $BUILD_DIR \
+                -DLLVM_ENABLE_PROJECTS="clang" \
+                -DCMAKE_BUILD_TYPE=Release \
+                -DLLVM_TARGETS_TO_BUILD="X86" \
+                -DCMAKE_CXX_COMPILER=clang++ \
+                -DCMAKE_C_COMPILER=clang \
+                -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+                -DCMAKE_INSTALL_PREFIX=${BUILD_DIR}/install
+        else
+            cmake -G "Ninja" \
+                -S ${LLVM_SOURCE_DIR}/llvm \
+                -B $BUILD_DIR \
+                -DLLVM_ENABLE_PROJECTS="clang" \
+                -DCMAKE_BUILD_TYPE=Release \
+                -DLLVM_TARGETS_TO_BUILD="X86" \
+                -DCMAKE_CXX_COMPILER=clang++ \
+                -DCMAKE_C_COMPILER=clang \
+                -DLLVM_BUILD_LLVM_DYLIB=ON \
+                -DLLVM_LINK_LLVM_DYLIB=ON \
+                -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+                -DCMAKE_INSTALL_PREFIX=${BUILD_DIR}/install
+        fi
         ninja -C $BUILD_DIR install
     fi
 }
 
 do_build()
 {
+    local BUILD_OPTIONS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+
     if [ "$BUILD_LLVM" = "yes" ]; then
         build_llvm $BUILD_LIBCLANG_DIR
         exit 0
@@ -48,10 +64,13 @@ do_build()
     rm -rf $BUILD_DIR
 
     if [ "$ENABLE_CXX" = "yes" ]; then
-        cmake -B $BUILD_DIR -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLANGUAGE_MODE_CXX=ON
-    else
-        cmake -B $BUILD_DIR -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+        BUILD_OPTIONS="$BUILD_OPTIONS -DLANGUAGE_MODE_CXX=ON"
     fi
+    if [ "$ENABLE_STATIC" = "yes" ]; then
+        BUILD_OPTIONS="$BUILD_OPTIONS -DENABLE_STATIC_BUILD=ON"
+    fi
+
+    cmake -B $BUILD_DIR ${BUILD_OPTIONS}
 
     make -C $BUILD_DIR $CLANG_TOOL
 }
@@ -68,6 +87,9 @@ do
         ;;
     -cxx  | --cxx )
         ENABLE_CXX="yes"
+        ;;
+    -static  | --static )
+        ENABLE_STATIC="yes"
         ;;
     *)
         echo "The parameter $1 is not allowed"
